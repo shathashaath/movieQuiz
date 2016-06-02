6e98b13f7de3ccfa893a9c587fe3b16bc35304e4
Alan Denison and shatha shaath <alan@>
2016-06-02 16:51:57 -0700

    add movieQuiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
+// $(document).ready(function() {
+// });
+// var result = $("input:radio[name=question1]:checked").val();
+
 $(document).ready(function() {
-});
+  $("form#answers").submit(function() {
+      event.preventDefault();
+      var q1 = parseInt($("input:radio[name=question1]:checked").val());
+      var q2 = parseInt($("input:radio[name=question2]:checked").val());
+      var q3 = parseInt($("input:radio[name=question3]:checked").val());
+      var total = q1+q2+q3;
+      alert(q1+q2+q3);
+    });
+  });
